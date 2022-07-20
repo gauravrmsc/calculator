c90bab545f4e9a2ded091572b295bb62f1ec1f53
Gaurav Kumar <=>
2022-07-20 09:43:57 +0530

    add Calculator Interface

--- src/main/java/org/calculator/service/Calculator.java
+++ src/main/java/org/calculator/service/Calculator.java
@@ -1,37 +1,13 @@
 package org.calculator.service;
 
 import org.calculator.domain.expression.Expression;
-import org.calculator.exception.CalculatorException;
-import org.calculator.exception.ExpressionParsingException;
-import org.calculator.exception.OperationNotSupportedException;
-import org.calculator.factory.OperatorFactory;
-import org.calculator.mapper.ExpressionMapper;
 
 /**
  * @author gauravkumar
- * @since 16/07/22
+ * @since 20/07/22
  */
-public class Calculator {
-    private final ExpressionMapper expressionMapper;
+public interface Calculator {
+    Double calculate(String expressionString);
 
-    public Calculator() {
-        this.expressionMapper = new ExpressionMapper();
-    }
-
-    public Double calculate(String expressionString) {
-        try {
-            Expression expression = expressionMapper.getExpression(expressionString);
-            return calculate(expression);
-        } catch (ExpressionParsingException e) {
-            throw new CalculatorException(e);
-        }
-    }
-
-    public Double calculate(Expression expression) {
-        try {
-            return OperatorFactory.getOperator(expression.getOpcode()).calculate(expression.getOperand1(), expression.getOperand2());
-        } catch (OperationNotSupportedException e) {
-            throw new CalculatorException(e);
-        }
-    }
+    Double calculate(Expression expression);
 }
